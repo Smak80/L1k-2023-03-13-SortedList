b1d3cd4efaae5a3c88436a8879e0cb7d1513fbfe
Sergey Makletsov <smaklets@kpfu.ru>
2023-03-13 13:02:30 +0300

    Функуия добавления элемента в список

--- L1k-2023-03-13-SortedList/sorted_list.cpp
+++ L1k-2023-03-13-SortedList/sorted_list.cpp
@@ -1,1 +1,33 @@
 #include "sorted_list.h"
+
+void add(sorted_list*& list, int value)
+{
+	list_elem* new_el = new list_elem;
+	new_el->x = value;
+	if (!list->first) list->first = list->last = new_el;
+	else
+	{
+		if (value <= list->first->x)
+		{
+			new_el->next = list->first;
+			list->first->prev = new_el;
+			list->first = new_el;
+		} else if (value >= list->last->x)
+		{
+			new_el->prev = list->last;
+			list->last->next = new_el;
+			list->last = new_el;
+		} else
+		{
+			auto* curr = list->first;
+			while (curr->next->x < value)
+			{
+				curr = curr->next;
+			}
+			curr->next->prev = new_el;
+			new_el->next = curr->next;
+			curr->next = new_el;
+			new_el->prev = curr;
+		}
+	}
+}
